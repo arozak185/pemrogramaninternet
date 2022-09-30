76e08bc2837cddb12d7d71b3e631598c4e0f771a
arozak185 <76271044+arozak185@users.noreply.github.com>
2022-09-30 14:17:35 +0700

    Add files via upload
    pertemuan 1 latihan 1

---
+++ latihan1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="" method="post">
+    <label for="nama" id="nama">Masukan Nama: </label>
+    <input type="text"name="nama">
+    <button type ="submit"name="submit">Proses</button>
+</form>
+<?php
+    if (isset($_POST['submit'])) {
+        echo "Selamat Datang, " ."<b>" .$_POST{'nama'} ."</b>";
+        }
+?>
+</body>
+</html>
